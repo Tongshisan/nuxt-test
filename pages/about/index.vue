c6b908718a3f1a74bfb431ac03da33ec6d4cdfa5
lizhi <lizhi0781@gmail.com>
2024-11-24 21:34:33 +0800

    first commit

---
+++ pages/about/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+console.log('useFetch', useFetch);
+const { data: users, error } = await useFetch(
+	'https://jsonplaceholder.typicode.com/users'
+);
+console.log('users', users.value);
+</script>
+
+<template>
+	<div class="home">
+		<h1>About</h1>
+	</div>
+</template>
